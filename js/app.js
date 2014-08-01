3c700be6fa4f8091c67375413631ccbe27a6ae5d
vamshisuram <vamshi.suram25@gmail.com>
2014-08-01 11:00:42 +0530

    changes pushed, Fixes #1

--- js/app.js
+++ js/app.js
@@ -38,6 +38,10 @@
         el: $('#contacts'),
         initialize: function () {
             this.collection = new Directory(contacts);
+
+          //  this.$el.find("#filter").append(this.createSelect());
+            this.on("change:filterType", this.filterByType, this);
+            this.collection.on("reset", this.render, this)
             this.render();
         },
 
@@ -53,9 +57,72 @@
                 model: item
             });
             this.$el.append(contactView.render().el);
+        },
+
+        getTypes: function () {
+            return _.uniq(this.collection.pluck("type"), false, function (type) {
+                return type.toLowerCase();
+            })
+        },
+
+        createSelect: function () {
+            var filter = this.$el.find("#filter"),
+                select = $("<select/>", { html: "<option>All</option>" });
+
+            _.each(this.getTypes(), function (item) {
+                var option = $("<option/>", {
+                    value: item.toLowerCase(),
+                    text: item.toLowerCase()
+                }).appendTo(select);
+            });
+
+            return select;
+        },
+
+        events: {
+            "change #filter select": "setFilter"
+        },
+
+        setFilter: function (e) {
+            this.filterType = e.currentTarget.value;
+            this.trigger("change: filterType")
+        },
+
+        filterByType: function () {
+            if (this.filterType === "all" ) {
+                this.collection.reset(contacts);
+                <b>contactsRouter.navigate("filter/all");</b>
+            } else {
+                this.collection.reset(contacts, {silent: true});
+
+                var filterType = this.filterType,
+                    filtered = _.filter(this.collection.models, function (item) {
+                        return item.get("type").toLowerCase() === filterType;
+                    })
+                this.collection.reset(filtered);
+
+                <b>contactsRouter.navigate("filter/" + filterType); </b>
+            }
         }
+
+
     });
 
     var directory = new DirectoryView();
+
+    var ContactsRouter = Backbone.Router.extend({
+        routes: {
+            "filter/:type": "urlFilter"
+        },
+
+        urlFilter: function (type) {
+            directory.filterType = type;
+            directory.trigger("change:filterType");
+        }
+    });
+
+    var contactsRouter = new ContactsRouter();
+    Backbone.history.start();
+
 
 } (jQuery));
